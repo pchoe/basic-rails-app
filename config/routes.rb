31500e4e40b58a26c232d0f0d68127f0521fd08b
pchoe <peter_choe@outlook.com>
2015-01-01 07:32:26 +0000

    Pushed to Production

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Bloccit::Application.routes.draw do
   get 'topics/edit'
 
   devise_for :users
+  resources :users, only: [:update]
   
   resources :topics do
     resources :posts, except: [:index]
